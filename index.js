d1951a3a5bb6bad2320b2bf28e6ba05e63457e4c
nikom2532 <nikom2532@gmail.com>
2019-04-14 17:27:59 +0700

    Update1

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Hello World');
+console.log('Hello World')
+
+
+var id = 3 // Number
+var name = 'Artdvp' // String
+var ok = true // Boolean
+
+var name2 = `Artdvp id = ${id}` // การทำ String ให้สามารถใส่ตัวแปรหรือ expression (Template literals) ครอบด้วย `...`
+
+// Array, Object, Function
+// Object
+/*
+var Object = {
+  key: 'value'
+}
+*/
+var student = {
+  code: '001',
+  name: 'Artdvp'
+}
+
+// แสดงค่า
+console.log(student.code) // 001
+console.log(student.name) // Artdvp
+// เพิ่ม key ใหม่
+student['age'] = 25
+// ลบ key 
+delete student.age
+
+// Array
+// เก็บข้อมูลใน array เป็นชนิดใดก็ได้
+var list = ['aaa',1 , {id: 1}, ['AAAA','BBBB']]
+
+for (let i = 0; i < list.length; i++) {
+    const element = list[i];
+    console.log(element);
+}
+/*
+result:
+'aaa'
+1
+{ id: 1 }
+['AAAA','BBBB']
+*/
